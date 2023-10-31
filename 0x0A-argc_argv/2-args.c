714eb6066b847fd64bf9df3aad9e3717d0f406ec
Najmmohmed12 <najmmohmmed@outlook.com>
2023-10-31 05:08:46 -0700

    1,2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,17 +5,11 @@
 
 
 /**
-
  * main - prints all arguments it receives
-
  * @argc: number of arguments
-
  * @argv: array of arguments
-
  *
-
  * Return: Always 0 (Success)
-
  */
 
 int main(int argc, char *argv[])
@@ -39,3 +33,4 @@ int main(int argc, char *argv[])
 	return (0);
 
 }
+
